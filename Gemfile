fe3a00b0be4a70c79c54a201c659e22789b6d883
Zachary Latta <zchlatta@gmail.com>
2013-05-10 03:12:04 -0700

    Added Google analytics tracking

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'therubyracer'
 gem 'less-rails'
 gem 'twitter-bootstrap-rails'
 
+gem 'google-analytics-rails'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ Blog::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  GA.tracker = "UA-34529482-3"
 end
